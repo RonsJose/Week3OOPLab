070ec99185ca4ec12a2deb75aa58d15041540779
RonsJose-STUDENT <g00464215@atu.ie>
2025-09-29 09:49:12 +0100

    Added feature power and modulo

--- src/main/java/ie/atu/MainApp.java
+++ src/main/java/ie/atu/MainApp.java
@@ -7,15 +7,19 @@ public class MainApp {
         Scanner scan=new Scanner(System.in);
 
        System.out.println("Basic calculator\n");
+
+       //Getting the numbers
         System.out.println("Enter 1st number: ");
         int FirstNumber=scan.nextInt();
         System.out.println("Enter 2nd number: ");
         int SecondNumber=scan.nextInt();
 
-       System.out.println("Please pick one of the options:\n\n1. Addition\n2. Subtraction\n3.Multiplication\n4.Division");
+        //Getting operator
+       System.out.println("Please pick one of the options:\n\n1. Addition\n2. Subtraction\n3.Multiplication\n4.Division\n5.Power\n6.Modulo");
        int operation=scan.nextInt();
         Calculator calc=new Calculator();
 
+        //Depending on operator calling the class using object calc and using the specified method
        double result=switch(operation)
         {
             case 1 -> {
@@ -28,6 +32,7 @@ public class MainApp {
                 yield calc.mul(FirstNumber,SecondNumber);
             }
             case 4 ->{
+                //Checking for divide by 0 error
                 if(SecondNumber==0)
                 {
                     System.out.println("Can't divide by zero");
@@ -38,12 +43,21 @@ public class MainApp {
                     yield calc.div(FirstNumber,SecondNumber);
                 }
             }
+            case 5->
+            {
+                yield calc.power(FirstNumber,SecondNumber);
+            }
+            case 6 ->
+            {
+                yield calc.modulo(FirstNumber,SecondNumber);
+            }
             default -> {
               System.out.println("Invalid input");
               yield 0;
             }
         };
 
+       //Print out the answer
        System.out.println("\nThe answer is: "+result);
 
         scan.close();
